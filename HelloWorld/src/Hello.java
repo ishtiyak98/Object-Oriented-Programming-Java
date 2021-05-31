ee2f204a910963997e65ad7044c511660d9d4bd5
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2021-06-01 02:24:50 +0600

    1st code

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,18 @@
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("!--Hello_World--!");
+        System.out.println("Name : Rifat Ishtiyak");
+        System.out.println("ID : 18-37927-2");
+
+        int a=10;
+        int b=10;
+
+        System.out.println("");
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(b++);
+        System.out.println(b++ + ++b);
+
+    }
+}
