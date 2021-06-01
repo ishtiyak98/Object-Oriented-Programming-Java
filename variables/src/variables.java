33ddb96b3faedb2580c7560797d2007c87e4f2e3
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2021-06-02 02:02:18 +0600

    variables, type-casting

---
+++ variables/src/variables.java
@@ -0,0 +1,13 @@
+public class variables {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("Sum is : "+c);
+
+        float f=18.567f;
+        double g=34.578697;
+        int d=(int)g;    //typecasting
+        System.out.println(d);
+    }
+}
